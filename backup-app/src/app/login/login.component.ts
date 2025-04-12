768b29857b067b969b3b685d85791850ff336fa4
kamdem arielle <arielle.kamdem@pether.io>
2025-04-12 17:22:38 +0100

    updated the login page

--- backup-app/src/app/login/login.component.ts
+++ backup-app/src/app/login/login.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule,FormBuilder, Validators } from '@angular/forms';
+import { CoreService } from '../services/core.service';
+
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NgClass,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  public username: string | null = null;
+  public date: Date = new Date();
+  public passwordInputTextHidden: boolean = true;
+  public loading = false;
+ 
+
+  private formBuilder = inject(FormBuilder);
+
+  public loginForm =  this.formBuilder.group({
+    username: ["", Validators.required],
+    password: ["", Validators.required],
+  });;
+
+  constructor(private core: CoreService) {}
+
+  onSubmit() {}
+
+
+  togglePasswordVisibility(){}
+
+  changeLang(language: string) {
+
+  }
 }
